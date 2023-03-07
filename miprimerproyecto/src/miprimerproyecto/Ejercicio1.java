b71b75b87ddf873222118a98a09c6f3dba28563a
ariellendez2 <ariellendez@gmail.com>
2023-03-06 21:30:00 -0300

    Create Ejercicio1.java
    Subo Ejercicio 1 Hola Mundo

---
+++ miprimerproyecto/src/miprimerproyecto/Ejercicio1.java
@@ -0,0 +1,11 @@
+package miprimerproyecto;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		// TODO Auto-generated method stub
+
+	}
+
+}
